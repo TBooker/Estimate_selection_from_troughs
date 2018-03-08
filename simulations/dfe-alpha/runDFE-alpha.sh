ca1f153c018d836ac996fcd3e573149d2abe3032
TBooker <T.R.Booker@sms.ed.ac.uk>
2018-03-08 17:22:49 +0000

    3-epoch models

--- simulations/dfe-alpha/runDFE-alpha.sh
+++ simulations/dfe-alpha/runDFE-alpha.sh
@@ -1,13 +1,13 @@
-for i in Nes200_dDFE.Exponential_div10 Nes200_dDFE.Exponential Nes10_dDFE.Exponential_div10 Nes10_dDFE.Exponential Exp.Nes10_dDFE.Exponential_div10 Exp.Nes10_dDFE.Exponential Exp.Nes100_dDFE.Exponential_div10 Exp.Nes100_dDFE.Exponential
+for i in Nes200_dDFE.Exponential_div10 Nes200_dDFE.Exponential Nes10_dDFE.Exponential_div10 Nes10_dDFE.Exponential Exp.Nes10_dDFE.Exponential_div10 Exp.Nes10_dDFE.Exponential Exp.Nes100_dDFE.Exponential_div10 Exp.Nes100_dDFE.Exponential BimodalDFE BimodalDFE_div10 BimodalDFE_div100
 do
 
-mkdir $i
-mkdir $i/pointEstimate
-mkdir $i/pointEstimate/1-epoch
-mkdir $i/pointEstimate/2-epoch
+#mkdir $i
+#mkdir $i/pointEstimate
+#mkdir $i/pointEstimate/1-epoch
+#mkdir $i/pointEstimate/2-epoch
 mkdir $i/pointEstimate/3-epoch
-python ../../bin/generateInputFiles.py -i ../$i/$i.func.sfs -b point -o $i/pointEstimate/sfs.txt -s m6
-echo $i
+#python ../../bin/generateInputFiles.py -i ../$i/$i.func.sfs -b point -o $i/pointEstimate/sfs.txt -s m6
+#echo $i
 sh ../../bin/runDFEalphaOnSFS.sh $i
 
 #    for j in $(seq 1 10)
