2adeb07937b0cebd14f94d8512f68837fc6d9679
Tom Booker <t.r.booker@sms.ed.ac.uk>
2018-05-01 09:08:27 -0700

    added analysis script

---
+++ simulations/NewSimulations/NeSaPa0.5/run.sh
@@ -0,0 +1,48 @@
+../../../poly_dfe/polyDFE/polyDFE -d analysis/Ns400_pa_0.00125_polyDFE.txt -i ../../../poly_dfe/polyDFE/input/init_model_BandC.txt 1 -r ../../../poly_dfe/polyDFE/input/range_model_BandC.txt 1000 -m B > analysis/Ns400_pa_0.00125_polyDFE.fullDFE.withDiv.out &
+../../../poly_dfe/polyDFE/polyDFE -d analysis/Ns400_pa_0.00125_polyDFE.txt -i ../../../poly_dfe/polyDFE/input/init_model_BandC.txt 2 -r ../../../poly_dfe/polyDFE/input/range_model_BandC.txt 1000 -m B > analysis/Ns400_pa_0.00125_polyDFE.dDFE.withDiv.out &
+
+../../../poly_dfe/polyDFE/polyDFE -d analysis/Ns400_pa_0.00125_polyDFE.txt -i ../../../poly_dfe/polyDFE/input/init_model_BandC.txt 1 -r ../../../poly_dfe/polyDFE/input/range_model_BandC.txt 1000 -m B -w > analysis/Ns400_pa_0.00125_polyDFE.fullDFE.noDiv.out &
+../../../poly_dfe/polyDFE/polyDFE -d analysis/Ns400_pa_0.00125_polyDFE.txt -i ../../../poly_dfe/polyDFE/input/init_model_BandC.txt 2 -r ../../../poly_dfe/polyDFE/input/range_model_BandC.txt 1000 -m B -w > analysis/Ns400_pa_0.00125_polyDFE.dDFE.noDiv.out &
+
+
+../../../poly_dfe/polyDFE/polyDFE -d analysis/Ns200_pa_0.0025_polyDFE.txt -i ../../../poly_dfe/polyDFE/input/init_model_BandC.txt 1 -r ../../../poly_dfe/polyDFE/input/range_model_BandC.txt 1000 -m B > analysis/Ns200_pa_0.0025_polyDFE.fullDFE.withDiv.out &
+../../../poly_dfe/polyDFE/polyDFE -d analysis/Ns200_pa_0.0025_polyDFE.txt -i ../../../poly_dfe/polyDFE/input/init_model_BandC.txt 2 -r ../../../poly_dfe/polyDFE/input/range_model_BandC.txt 1000 -m B > analysis/Ns200_pa_0.0025_polyDFE.dDFE.withDiv.out &
+
+../../../poly_dfe/polyDFE/polyDFE -d analysis/Ns200_pa_0.0025_polyDFE.txt -i ../../../poly_dfe/polyDFE/input/init_model_BandC.txt 1 -r ../../../poly_dfe/polyDFE/input/range_model_BandC.txt 1000 -m B -w > analysis/Ns200_pa_0.0025_polyDFE.fullDFE.noDiv.out &
+../../../poly_dfe/polyDFE/polyDFE -d analysis/Ns200_pa_0.0025_polyDFE.txt -i ../../../poly_dfe/polyDFE/input/init_model_BandC.txt 2 -r ../../../poly_dfe/polyDFE/input/range_model_BandC.txt 1000 -m B -w > analysis/Ns200_pa_0.0025_polyDFE.dDFE.noDiv.out &
+
+
+../../../poly_dfe/polyDFE/polyDFE -d analysis/Ns100_pa_0.005_polyDFE.txt -i ../../../poly_dfe/polyDFE/input/init_model_BandC.txt 1 -r ../../../poly_dfe/polyDFE/input/range_model_BandC.txt 1000 -m B > analysis/Ns100_pa_0.005_polyDFE.fullDFE.withDiv.out &
+../../../poly_dfe/polyDFE/polyDFE -d analysis/Ns100_pa_0.005_polyDFE.txt -i ../../../poly_dfe/polyDFE/input/init_model_BandC.txt 2 -r ../../../poly_dfe/polyDFE/input/range_model_BandC.txt 1000 -m B > analysis/Ns100_pa_0.005_polyDFE.dDFE.withDiv.out &
+
+../../../poly_dfe/polyDFE/polyDFE -d analysis/Ns100_pa_0.005_polyDFE.txt -i ../../../poly_dfe/polyDFE/input/init_model_BandC.txt 1 -r ../../../poly_dfe/polyDFE/input/range_model_BandC.txt 1000 -m B -w > analysis/Ns100_pa_0.005_polyDFE.fullDFE.noDiv.out &
+../../../poly_dfe/polyDFE/polyDFE -d analysis/Ns100_pa_0.005_polyDFE.txt -i ../../../poly_dfe/polyDFE/input/init_model_BandC.txt 2 -r ../../../poly_dfe/polyDFE/input/range_model_BandC.txt 1000 -m B -w > analysis/Ns100_pa_0.005_polyDFE.dDFE.noDiv.out &
+
+
+../../../poly_dfe/polyDFE/polyDFE -d analysis/Ns50_pa_0.01_polyDFE.txt -i ../../../poly_dfe/polyDFE/input/init_model_BandC.txt 1 -r ../../../poly_dfe/polyDFE/input/range_model_BandC.txt 1000 -m B > analysis/Ns50_pa_0.01_polyDFE.fullDFE.withDiv.out &
+../../../poly_dfe/polyDFE/polyDFE -d analysis/Ns50_pa_0.01_polyDFE.txt -i ../../../poly_dfe/polyDFE/input/init_model_BandC.txt 2 -r ../../../poly_dfe/polyDFE/input/range_model_BandC.txt 1000 -m B > analysis/Ns50_pa_0.01_polyDFE.dDFE.withDiv.out &
+
+../../../poly_dfe/polyDFE/polyDFE -d analysis/Ns50_pa_0.01_polyDFE.txt -i ../../../poly_dfe/polyDFE/input/init_model_BandC.txt 1 -r ../../../poly_dfe/polyDFE/input/range_model_BandC.txt 1000 -m B -w > analysis/Ns50_pa_0.01_polyDFE.fullDFE.noDiv.out &
+../../../poly_dfe/polyDFE/polyDFE -d analysis/Ns50_pa_0.01_polyDFE.txt -i ../../../poly_dfe/polyDFE/input/init_model_BandC.txt 2 -r ../../../poly_dfe/polyDFE/input/range_model_BandC.txt 1000 -m B -w > analysis/Ns50_pa_0.01_polyDFE.dDFE.noDiv.out &
+
+
+../../../poly_dfe/polyDFE/polyDFE -d analysis/Ns25_pa_0.02_polyDFE.txt -i ../../../poly_dfe/polyDFE/input/init_model_BandC.txt 1 -r ../../../poly_dfe/polyDFE/input/range_model_BandC.txt 1000 -m B > analysis/Ns25_pa_0.02_polyDFE.fullDFE.withDiv.out &
+../../../poly_dfe/polyDFE/polyDFE -d analysis/Ns25_pa_0.02_polyDFE.txt -i ../../../poly_dfe/polyDFE/input/init_model_BandC.txt 2 -r ../../../poly_dfe/polyDFE/input/range_model_BandC.txt 1000 -m B > analysis/Ns25_pa_0.02_polyDFE.dDFE.withDiv.out &
+
+../../../poly_dfe/polyDFE/polyDFE -d analysis/Ns25_pa_0.02_polyDFE.txt -i ../../../poly_dfe/polyDFE/input/init_model_BandC.txt 1 -r ../../../poly_dfe/polyDFE/input/range_model_BandC.txt 1000 -m B -w > analysis/Ns25_pa_0.02_polyDFE.fullDFE.noDiv.out &
+../../../poly_dfe/polyDFE/polyDFE -d analysis/Ns25_pa_0.02_polyDFE.txt -i ../../../poly_dfe/polyDFE/input/init_model_BandC.txt 2 -r ../../../poly_dfe/polyDFE/input/range_model_BandC.txt 1000 -m B -w > analysis/Ns25_pa_0.02_polyDFE.dDFE.noDiv.out &
+
+
+../../../poly_dfe/polyDFE/polyDFE -d analysis/Ns10_pa_0.05_polyDFE.txt -i ../../../poly_dfe/polyDFE/input/init_model_BandC.txt 1 -r ../../../poly_dfe/polyDFE/input/range_model_BandC.txt 1000 -m B > analysis/Ns10_pa_0.05_polyDFE.fullDFE.withDiv.out &
+../../../poly_dfe/polyDFE/polyDFE -d analysis/Ns10_pa_0.05_polyDFE.txt -i ../../../poly_dfe/polyDFE/input/init_model_BandC.txt 2 -r ../../../poly_dfe/polyDFE/input/range_model_BandC.txt 1000 -m B > analysis/Ns10_pa_0.05_polyDFE.dDFE.withDiv.out &
+
+../../../poly_dfe/polyDFE/polyDFE -d analysis/Ns10_pa_0.05_polyDFE.txt -i ../../../poly_dfe/polyDFE/input/init_model_BandC.txt 1 -r ../../../poly_dfe/polyDFE/input/range_model_BandC.txt 1000 -m B -w > analysis/Ns10_pa_0.05_polyDFE.fullDFE.noDiv.out &
+../../../poly_dfe/polyDFE/polyDFE -d analysis/Ns10_pa_0.05_polyDFE.txt -i ../../../poly_dfe/polyDFE/input/init_model_BandC.txt 2 -r ../../../poly_dfe/polyDFE/input/range_model_BandC.txt 1000 -m B -w > analysis/Ns10_pa_0.05_polyDFE.dDFE.noDiv.out &
+
+
+../../../poly_dfe/polyDFE/polyDFE -d analysis/Ns5_pa_0.1_polyDFE.txt -i ../../../poly_dfe/polyDFE/input/init_model_BandC.txt 1 -r ../../../poly_dfe/polyDFE/input/range_model_BandC.txt 1000 -m B > analysis/Ns5_pa_0.1_polyDFE.fullDFE.withDiv.out &
+../../../poly_dfe/polyDFE/polyDFE -d analysis/Ns5_pa_0.1_polyDFE.txt -i ../../../poly_dfe/polyDFE/input/init_model_BandC.txt 2 -r ../../../poly_dfe/polyDFE/input/range_model_BandC.txt 1000 -m B > analysis/Ns5_pa_0.1_polyDFE.dDFE.withDiv.out &
+
+../../../poly_dfe/polyDFE/polyDFE -d analysis/Ns5_pa_0.1_polyDFE.txt -i ../../../poly_dfe/polyDFE/input/init_model_BandC.txt 1 -r ../../../poly_dfe/polyDFE/input/range_model_BandC.txt 1000 -m B -w > analysis/Ns5_pa_0.1_polyDFE.fullDFE.noDiv.out &
+../../../poly_dfe/polyDFE/polyDFE -d analysis/Ns5_pa_0.1_polyDFE.txt -i ../../../poly_dfe/polyDFE/input/init_model_BandC.txt 2 -r ../../../poly_dfe/polyDFE/input/range_model_BandC.txt 1000 -m B -w > analysis/Ns5_pa_0.1_polyDFE.dDFE.noDiv.out &
+
