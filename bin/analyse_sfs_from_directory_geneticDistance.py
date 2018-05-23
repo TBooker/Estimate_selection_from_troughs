f2944e635fb5239c6145824ff32540391a249a65
Tom Booker <t.r.booker@sms.ed.ac.uk>
2018-05-23 11:40:05 -0700

    updated the analysis script to use log binning for the different elements

--- bin/analyse_sfs_from_directory_geneticDistance.py
+++ bin/analyse_sfs_from_directory_geneticDistance.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
 import math, argparse, gzip, glob
 import site_frequency_spectrum as SFS
-import pandas as pd
+import pandas as pd, numpy as np
 from tom import brace, bootstrap_sample
+
+def find_nearest(array, value):
+    array = np.asarray(array)
+    idx = (np.abs(array - value)).argmin()
+    if value - array[idx] >0: return array[idx]
+    elif value - array[idx] < 0 : return array[idx-1]
+
 
 def roundup(x, by):
     return int(math.ceil(x / (by*1.))) * (by*1.)
@@ -27,29 +34,32 @@ def get_full_sfs(sfs_dict_raw):
 			if not s: continue
 			full_sfs = SFS.merge_SFS(full_sfs, s)
 		grand[i] = full_sfs
+	
 	return grand
 
 def get_summary(sfs_dict, label):
 	out = []
 	for key in sfs_dict.keys():
-		if key.split('.')[0] == 'u':
-			mult = -1
-		elif key.split('.')[0] == 'd':
-			mult = 1
-
-		start = int(key)-1
-		end = int(key)
-		mid = (end+start)/2
-
-		out.append([label, key, start, mid, end, 
-				SFS.pi(sfs_dict[key]), 
-				SFS.xsi(sfs_dict[key]), 
-				SFS.pi2(sfs_dict[key]), 
-				SFS.fwh(sfs_dict[key]), 
-				SFS.theta_W(sfs_dict[key]), 
-				SFS.tajima(sfs_dict[key]),
-				SFS.KZl(sfs_dict[key]),
-				sum(sfs_dict[key])])	
+		mid = key
+		if sum(sfs_dict[key][1:-1]) == 0:
+			out.append([label, mid, 
+					'Nan', 
+					'Nan', 
+					'Nan', 
+					'Nan', 
+					'Nan', 
+					'Nan', 
+					sum(sfs_dict[key])])	
+		elif sum(sfs_dict[key][1:-1]) > 0:
+			out.append([label, mid, 
+					SFS.pi(sfs_dict[key]), 
+					SFS.xsi(sfs_dict[key]), 
+					SFS.pi2(sfs_dict[key]), 
+					SFS.fwh(sfs_dict[key]), 
+					SFS.theta_W(sfs_dict[key]), 
+					SFS.tajima(sfs_dict[key]),
+	#				SFS.KZl(sfs_dict[key]),
+					sum(sfs_dict[key])])	
 	return out
 
 def main():
@@ -87,6 +97,12 @@ def main():
 			action = "store_true", 
 			help = "Is the input file gzipped? It is recommended (by me) that the file should be gzipped",
 			default = False)
+	parser.add_argument("--cne", 
+			required = False,
+			dest = "cne", 
+			action = "store_true", 
+			help = "Analysing CNE?",
+			default = False)
 	args = parser.parse_args()
 	
 	if args.gz:
@@ -101,38 +117,49 @@ def main():
 #	comparison['pi_red']
 #
 #	brace()
-	
+	if args.cne:
+		roundBy = 2
+		bins = np.logspace(0, 2.477122, 100) - 1
+#		bins = range(0, 200, roundBy) ## For CNEs
+	else:
+		roundBy = 30
+		bins = np.logspace(0, 3.477122, 100) - 1
+#		bins = range(0, 3000, roundBy) ## For Exons
+
 	print 'opening file and creating dictionary of SFS by interval'
-	ranges = range(0, 3000, 30) ## For Exons
-	ranges = range(0, 200, 2) ## For CNEs
-	print ranges
-	for g in glob.glob(args.input + '/*'):
 		
-		print g
-		for i in open(g):
+	count = 0
+
+	for g in glob.glob(args.input + '/*'):
+		count +=1 
+		
+
+		for i in opener(g):
 			y = i.strip("\n").split("[")
 			dist = float(y[1].split(' ')[1].strip(','))
-			if dist < 0:
-				region = '0'
-			elif dist > 200:
-				region = '200'
-			else:
-				region = str( int(roundup(dist,2)) )
 
-#			region =  y[1].strip(", ").strip("''")
+			stream = y[1].split(',')[0].strip("'").split('.')[0]
+			if stream == 'u':
+				region = -1.*find_nearest(bins, dist) 
+				if region == 0.0:
+					region = -0.000000000001
+			elif stream == 'd':
+				region = find_nearest(bins, dist) 
+				if region == 0.0:
+					region = -0.000000000001
+
 			region_sfs =  map(int,y[2].strip("]]").split(",")) 
-
 			try:
 				sfs_dict_raw[region].append(region_sfs)
 			except KeyError:
 				sfs_dict_raw[region] = [region_sfs]
-	header = ['label', 'key', 'start', 'mid', 'end', 'pi', 'xsi', 'pi2', 'fwh', 'theta_W', 'tajima' , 'KZl', 'sites'] 
+	header = ['label', 'mid', 'pi', 'xsi', 'pi2', 'fwh', 'theta_W', 'tajima' ,  'sites'] 
 
 	print 'Read in the data, now combining'
 	
 	grand_sfs = pd.DataFrame(get_summary( get_full_sfs(sfs_dict_raw) , 'real'), columns = header)
-	grand_sfs = grand_sfs.set_index(grand_sfs['key'].values)
-
+	grand_sfs = grand_sfs.set_index(grand_sfs['mid'].values)
+	print grand_sfs
 	if args.boots == 0:
 		
 		grand_sfs.to_csv(args.output, index = False)
@@ -155,19 +182,19 @@ def main():
 
 	### Could write the bootstrap dataset to a dataframe, and use that to get the confidence intervals in a separate script
 
-	for i in  set(bootstraps['key']):
+	for i in  set(bootstraps['mid']):
 		bootstrap_stats = {}
-		temp = bootstraps.ix[bootstraps['key']==i]
-		for stat in [ 'pi', 'xsi', 'pi2', 'fwh', 'theta_W', 'tajima' , 'KZl' ]:
+		temp = bootstraps.ix[bootstraps['mid']==i]
+		for stat in [ 'pi', 'xsi', 'pi2', 'fwh', 'theta_W', 'tajima' ,  ]:
 			bootstrap_stats[stat+'_lower'] = sorted(temp[stat])[int(args.boots*0.025)]
 			bootstrap_stats[stat+'_median'] = sorted(temp[stat])[int(args.boots*0.5)]
 			bootstrap_stats[stat+'_upper']  = sorted(temp[stat])[int(args.boots*0.975)]
-		bootstrap_stats['key'] = i
+		bootstrap_stats['mid'] = i
 		interval_stats.append(bootstrap_stats)
 
 	bootstrap_stats = pd.DataFrame.from_dict(interval_stats)
-	bootstrap_stats = bootstrap_stats.set_index(bootstrap_stats['key'].values)
-	full = pd.merge(bootstrap_stats, grand_sfs, on = 'key')
+	bootstrap_stats = bootstrap_stats.set_index(bootstrap_stats['mid'].values)
+	full = pd.merge(bootstrap_stats, grand_sfs, on = 'mid')
 	full.reindex_axis(sorted(full.columns), axis=1).to_csv('summary_'+args.output)
 
 
